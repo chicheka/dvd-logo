7ebcb52add24d487467d1a6c6c07b08f7c3d8bc5
Simeon Ivanov <simeonmcbg@gmail.com>
2023-01-17 18:46:08 +0200

    Corner hit counter, improved color randomizer, custom size

--- main.js
+++ main.js
@@ -1,55 +1,66 @@
-let getcan = document.getElementById("canvas");
-let canvas = getcan.getContext("2d");
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
 let dvd = document.getElementById("dvd");
+
 let x = 1;
 let y = 1;
 let xMove = 1;
 let yMove = 1;
+let corners = 0;
+
 document.getElementById("start").onclick = function(){
+    let width = Number(document.getElementById("width").value);
+    let height = Number(document.getElementById("height").value);
+    canvas.width = width;
+    canvas.height = height;
+
     setInterval(function(){
-        canvas.fillStyle = "#000000";
-        canvas.fillRect(0, 0, 600, 400);
-        canvas.drawImage(dvd, x, y);
-        if(x + 60 == 600){
-            xMove = -1;
+        context.fillStyle = "#000000";
+        context.fillRect(0, 0, width, height);
+        context.drawImage(dvd, x, y);
+        //bounce off
+        if(x + 60 == width || x == 0){
+            xMove *= -1;
             dvd = document.getElementById(randomColor());
         }
-        if(x == 0){
-            xMove = 1;
+        if(y + 27 == height || y == 0){
+            yMove *= -1;
             dvd = document.getElementById(randomColor());
         }
-        if(y + 27 == 400){
-            yMove = -1;
-            dvd = document.getElementById(randomColor());
-        }
-        if(y == 0){
-            yMove = 1;
-            dvd = document.getElementById(randomColor());
+        //corners
+        if((x + 60 == width || x == 0) && (y + 27 == height || y == 0)){
+            corners++;
+            document.getElementById("corners").innerHTML = "corners hit: " + corners;
         }
         x += xMove;
         y += yMove;
     }, 10);
 }
+
+let colorsAvailable = ["dvd-red", "dvd-yellow", "dvd-green", "dvd-cyan", "dvd-blue", "dvd-magenta"];
+
 function randomColor(){
-    let rand = Math.floor((Math.random() * 6) + 1);
-    switch(rand){
-        case 1:
-            return "dvd-red";
-            break;
-        case 2:
-            return "dvd-yellow";
-            break;
-        case 3:
-            return "dvd-green";
-            break;
-        case 4:
-            return "dvd-cyan";
-            break;
-        case 5:
-            return "dvd-blue";
-            break;
-        case 6:
-            return "dvd-magenta";
-            break;
+    let rand;
+    let current;
+    if (dvd.id != "dvd"){
+        //detect the current color
+        for (let i = 0; dvd.id != colorsAvailable[i - 1]; i++){
+            current = i;
+        }
+        rand = Math.floor((Math.random() * 5) + 1) - 1;// from 0 to 4
+        if(rand >= current) rand++;
+        /*
+         * if the current is green (2), then it won't be picked no matter what random number is chosen:
+         *  0 1 2 3 4   <-- random number
+         *  | | \ \ \
+         *  | |  \ \ \
+         *  | |   | | |
+         *  V V   V V V
+         *  0 1 2 3 4 5 <-- colors from red to magenta
+         * */
     }
+    //if logo is still white
+    else rand = Math.floor((Math.random() * 6) + 1) - 1;
+
+    return colorsAvailable[rand];
 }
